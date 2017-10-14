b4178dcb4335b34618ae27dafe641968bf6b31a6
sidd0103 <sidbiyer@gmail.com>
2017-10-14 09:44:36 -0700

    item generation started

--- js/marketplace.js
+++ js/marketplace.js
@@ -1,32 +1,40 @@
 $(document).ready(function () {
+    //handle filling in the item window
+    initItems();
+    
+    //handle the select
+    $('select').material_select();
+    
     /*Handle the Side Navigation Opening*/
     //check to see if the navs have been closed
     var navsClosed = false;
     mediaCheck({
         media: '(max-width: 992px)',
         entry: function () {
-            $('#openCartbtn').sideNav({
-                menuWidth: 300, // Default is 300
-                edge: 'right', // Choose the horizontal origin
-                closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                draggable: true, // Choose whether you can drag to open on touch screens,
-                onOpen: function (el) {}, // A function to be called when sideNav is opened
-                onClose: function (el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
-            });
-            $('#openNavbtn').sideNav({
-                menuWidth: 300, // Default is 300
-                edge: 'left', // Choose the horizontal origin
-                closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-                draggable: true, // Choose whether you can drag to open on touch screens,
-                onOpen: function (el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
-                onClose: function (el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
-            });
+            
         },
         exit: function () {
-            $('#openCartbtn').sideNav('destroy');
-            $('#openNavbtn').sideNav('destroy');
+            $('#sidenav-overlay').remove();
         }
     });
+    $('#openCartbtn').sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens,
+        onOpen: function (el) {}, // A function to be called when sideNav is opened
+        onClose: function (el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    });
+    $('#openNavbtn').sideNav({
+        menuWidth: 300, // Default is 300
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true, // Choose whether you can drag to open on touch screens,
+        onOpen: function (el) { /* Do Stuff*/ }, // A function to be called when sideNav is opened
+        onClose: function (el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    });
+    
+    
     /*Handle the Filters*/
     var slider = document.getElementById('test-slider');
     noUiSlider.create(slider, {
@@ -42,4 +50,5 @@ $(document).ready(function () {
             decimals: 0
         })
     });
+    
 })
