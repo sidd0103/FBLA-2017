506755f3094571d50ce136a98f5734775b9db5e1
sidd0103 <sidbiyer@gmail.com>
2018-02-09 16:18:40 -0800

    hotfix

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,27 @@ $(document).ready(function(){
         }
     })
 })
+
+//this callback function runs once the google maps library is fetched. 
+function initAutoComplete(){
+    //set the input and initialize the autofill
+    var input = document.getElementById('enterLocation');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    //when the submit button is clicked, we filter through the address to make sure its valid. 
+    $('.location-submit').click(function(){
+        var place = autocomplete.getPlace();
+        console.log(place);
+        if (place == null) {
+            Materialize.toast('<span class="error-icon material-icons">error</span>Please Enter a Location!',500,'toast-style');
+        }
+        else {
+            if (place['types'][0] == 'street_address' || place['types'][0] == 'premise') {
+                sessionStorage.setItem('deliveryLocation', place['formatted_address']);
+                window.location.href = 'marketplace.html'
+            }
+            else {
+                Materialize.toast('<span class="error-icon material-icons">error</span>We cannot deliver to this location.',500,'toast-style');
+            }
+        }
+    });
+}
